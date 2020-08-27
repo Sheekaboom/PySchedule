16dcd4100869c9b825c4d32a94a236a908914daf
Sheekaboom <aweiss7791@gmail.com>
2020-08-27 13:25:21 -0600

    basic implementation seems to be working

--- pyschedule/Schedule.py
+++ pyschedule/Schedule.py
@@ -6,7 +6,17 @@
 
 @author: aweis
 """
-from pyschedule.Tasks import Task
+
+import datetime
+
+#libs for exporting
+try:
+    import plotly.express as px #REQUIRES PLOTLY 4.9 or ABOVE
+except ModuleNotFoundError:
+    print("Plotly Not Available")
+
+
+from pyschedule.Tasks import Task, Dependency
 
 class Schedule(dict):
     '''
@@ -20,9 +30,10 @@ class Schedule(dict):
             - String - a path to a schedule to load in
     @param[in/OPT] kwargs - key/value pairs to be added as metadata
     '''
-    def __init__(*args,**kwargs):
+    def __init__(self,*args,**kwargs):
+        super().__init__()
         if isinstance(args[0],Schedule): #instantiate as a class
-            super().__init__(Schedule,**kwargs)
+            self.update(Schedule)
         else:
             self['tasks'] = []
             if isinstance(args[0],list):
@@ -32,16 +43,82 @@ class Schedule(dict):
                 self.add_task(args[0])
             else:
                 raise TypeError("Unsupported input arguments")
+        self.update(kwargs)
             
-    def add_task(task,**kwargs):
+    def add_task(self,task,**kwargs):
         '''
         @brief add a Task to the schedule
-        @param[in] task - Task to add to theschedule
+        @param[in] task - Task to add to the schedule
         @param[in] kwargs - key/value pairs
         @note, this adds the task to the list at
         '''
         if not isinstance(task,Task):raise TypeError
+        deps = task['dependencies']
+        
         self['tasks'].append(task)
         
+    def sort_tasks(self,**kwargs):
+        '''
+        @brief sort tasks based on start time
+        '''
+        end_sort = sorted(self['tasks'],key=lambda task: task.end) # sort by end date first
+        self['tasks'] = sorted(end_sort,key=lambda task: task.start)
+        self['tasks'] = self['tasks'].sort(key=lambda task: task.start)
+    
+#%% Export functionality
+    def _get_as_lists(self):
+        '''
+        @brief get fields from the schedule as lists. Typically useful for exporting/plotting
+        @return dict('name':[names],'start':[task.start],'end':[task.end],
+                     'percent_complete':[task['progress']['percent']])
+        '''
+        rd = {}
+        rd['name'] = [task['name'] for task in self['tasks']]
+        rd['start'] = [task.start for task in self['tasks']]
+        rd['end'] = [task.end for task in self['tasks']]
+        rd['percent_complete'] = [task['progress']['percent'] for task in self['tasks']]
+        return rd
+
+    def plot_plotly(self,**kwargs):
+        '''@brief plot the gantt chart with plotly'''
+        info_lists = self._get_as_lists()
+        fig = px.timeline(x_start=info_lists['start'],x_end=info_lists['end'], 
+                          y=info_lists['name'], color=info_lists['percent_complete'])
+        fig.update_yaxes(autorange="reversed")
+        fig.update_layout(
+            shapes=[dict(x0=datetime.datetime.today(),
+                         x1=datetime.datetime.today(),
+                         y0=0,y1=1,xref='x',yref='paper',line_width=2)],
+            annotations=[dict(x=datetime.datetime.today(), y=1.1, xref='x',
+                              yref='paper',font=dict(color="black",size=14),
+                              showarrow=False, xanchor='left', 
+                              text='Today ({})'.format(datetime.datetime.today()))]
+            )
+        
+        return fig
+        
             
-            
+#%% Testing
+import unittest
+
+class TestSchedule(unittest.TestCase):
+    '''@brief test scheduling things'''
+    pass
+
+if __name__=='__main__':
+
+    t1 = Task('task1',start=datetime.datetime.today()-datetime.timedelta(days=5)
+              ,duration=datetime.timedelta(days=7))
+    t2 = Task('task2',duration=datetime.timedelta(days=14))
+    t3 = Task('task3',duration=datetime.timedelta(days=31))
+    t4 = Task('task3',duration=datetime.timedelta(days=31))
+    
+    t2.add_dependency(Dependency(t1,'startsAfter'))
+    t3.add_dependency(Dependency(t1,'startsAfter'))
+
+    mySchedule = Schedule([t1,t2,t3])
+    fig = mySchedule.plot_plotly()
+    fig.show(renderer='svg')
+
+
+     
--- pyschedule/Tasks.py
+++ pyschedule/Tasks.py
@@ -8,6 +8,7 @@
 """
 
 import datetime
+import numpy as np
 
 class Task(dict):
     '''
@@ -21,24 +22,90 @@ class Task(dict):
         - start - start date/time 
         - end   - end date/time
         - duration - duration in datetime.timedelta or '%d days, %H:%M:%S'
-        - dependendencies = [list of Dependency Objects]
+        - dependencies = [list of Dependency Objects]
         - progress - {percent:(0-100),notes: what the progress is}
         - risks - [{percent:possibility of failure(0-100),notes: whats the risk}]
         - children - [list of Tasks that are part of this task]
-        - todo - [names of subtasks that need to be completed]
+        - todo - [list of Task objects that need to be completed]
         - deliverables - [list of deliverables that will come out of this task]
         - contributions - [what contributions these tasks will provide]
         - resources - [list of Resources that this requires]
     '''
-    def __init__(self,name,**kwargs):
+    def __init__(self,name:str,start:datetime.datetime=None,end:datetime.datetime=None,
+                 duration:datetime.timedelta=None,**kwargs):
         '''@brief constructor'''
-        super().__init__({'name':name})
+        super().__init__({'name':name,'start':start,'end':end,'duration':duration})
+        # add our defaults
+        defaults = {
+            'dependencies':[],
+            'children':[],
+            'progress':{'percent':0,'notes':None},
+            'risks': [],
+            'todo': [],
+            'deliverables': [],
+            'contributions': [],
+            'resources': [],
+            }
+        self.update(defaults)
+        #update from inputs
+        self.update(kwargs)
+        
+    def add_dependency(self,dep,update=True):
+        '''@brief add a dependency object to the task'''
+        self['dependencies'].append(dep)
+        if update: #update our dates when a dependency is added
+            self.update_from_dependencies()
+        
+    def update_from_dependencies(self,verify=True):
+        '''@brief update times based on dependencies'''
+        dep_times = {'start':None,'end':None,'duration':None}
+        dvals = {}
+        for d in self['dependencies']:
+            dvals.update(d.get_dependency())
+        self.update(dvals)
+        if verify: #verify our times
+            self.verify()
+            
+    def verify(self):
+        '''@brief verify the task times are not overdefined'''
+        sed_vals = [self.get(k,None) for k in ['start','end','duration']]
+        num_nuns = len([v for v in sed_vals if v is None]) #number of None values
+        if num_nuns > 1: 
+            raise Exception("Underdefined times, this could be due to not updating dependencies.")
+        if num_nuns < 1:
+            raise Exception("Overdefined times, Start/End/Duration cannot all be defined.")
+        
+    # properties to return times if not provided
+    @property
+    def start(self):
+        '''@brief return the start datetime'''
+        start = self.get('start',None)
+        if start is None:
+            start = self.end-self.duration
+        return start 
+    @property
+    def end(self):
+        '''@brief return end datetime'''
+        end = self.get('end',None)
+        if end is None:
+            end = self.start+self.duration
+        return end
+    @property
+    def duration(self):
+        '''@brief return duration timedelta'''
+        dur = self.get('duration',None)
+        if dur is None:
+            dur = self.end-self.start
+        return dur
     
+    def __str__(self):
+        '''@brief return name for string'''
+        return self['name']
 
 class Dependency(dict):
     '''
     @brief dependencies tasks may have on one another
-    @param[in] dep_name - name of Task that we are dependent on
+    @param[in] dep_task - task object to be dependent on
     @param[in] dep_type - type of dependency. Can be any of the following:
         - startsBefore - Must start before the dep_name task begins
         - startsWith   - Must start at the same time as dep_name task
@@ -48,36 +115,61 @@ class Dependency(dict):
         - endsAfter    - Must end after dep_name ends
     @note the most likely type will be startsAfter
     '''
-    def __init__(self,dep_name,dep_type):
+    def __init__(self,dep_task:Task,dep_type:str):
         '''@brief constructor'''
         allowed_types = ['startsBefore','startsWith','startsAfter',
                          'endsBefore'  ,'endsWith'  ,'endsAfter'  ]
         if dep_type not in allowed_types:
             raise Exception("{} is not an allowable type of dependency".format(dep_type))
-        super().__init__(name=dep_name,type=dep_type)
+        super().__init__(task=dep_task,type=dep_type)
     
-    def get_dependency(self,task):
+    def get_dependency(self):
         '''@brief get a dependency time given a task'''
-        return getattr('_'+self['type'])(task);
+        return getattr(self,'_'+self['type'])(self['task']);
     
     def _startsBefore(self,task):
         '''@brief calculate date on startsBefore'''
-        return {'start':task['start'],'end':None,'duration':None}
+        return {'start':task.start}
     def _startsWith(self,task):
         '''@brief calculate date on startsWith'''
-        return {'start':task['start'],'end':None,'duration':None}
+        return {'start':task.start}
     def _startsAfter(self,task):
         '''@brief calculate date on startsBefore'''
-        return {'start':task['end'],'end':None,'duration':None}
+        return {'start':task.end}
     def _endsBefore(self,task):
         '''@brief calculate date on endsBefore'''
-        return {'start':None,'end':task['start'],'duration':None}
+        return {'end':task.start}
     def _endsWith(self,task):
         '''@brief calculate date on endsWith'''
-        return {'start':None,'end':task['end'],'duration':None}
+        return {'end':task.end}
     def _endsAfter(self,task):
         '''@brief calculate date on endsAfter'''
-        return {'start':None,'end':task['end'],'duration':None}
+        return {'end':task.end}
+    
+#%% Unit testing
 
+import unittest
+
+class TestTasks(unittest.TestCase):
+    start_datetime = datetime.datetime.today()
+    start_duration = datetime.timedelta(days=7)
+    task_len_days = [1,10,150,27,4]
+    time_deltas = [datetime.timedelta(days=d) for d in task_len_days]
+    
+    def test_cascaded_tasks(self):
+        '''@brief test each task starting after the other'''
+        start_task = Task('task_start',start=self.start_datetime,duration=start_duration)
+        other_tasks = [Task('task_{}'.format(i),duration=td) for i,td in enumerate(self.time_deltas)]
+
+if __name__=='__main__':
+    
+    t1 = Task('task1',start=datetime.datetime.today()
+              ,duration=datetime.timedelta(days=7))
+    t2 = Task('task2',duration=datetime.timedelta(days=14))
+    t3 = Task('task3',duration=datetime.timedelta(days=31))
+    
+    t2.add_dependency(Dependency(t1,'startsAfter'))
+    t3.add_dependency(Dependency(t2,'startsAfter'))
+    
 
 
